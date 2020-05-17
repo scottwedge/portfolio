ad8bcbc2d5c8e8c0b73a147fbc86e38b38d6dfaf
Trey Roche <proche@luc.edu>
2020-05-17 12:51:26 -0500

    Add build

---
+++ build/precache-manifest.462d9aa255a4aa2bcf359abf506663f1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f79b80b27e252e8b902b5d3f4d331455",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71ce73f3189640ef8809",
+    "url": "/static/css/main.9cd1a6a3.chunk.css"
+  },
+  {
+    "revision": "8437a9577d078c8187d2",
+    "url": "/static/js/2.a580c281.chunk.js"
+  },
+  {
+    "revision": "03b80fa8fe4a53d7fcc3e612b3bdb43b",
+    "url": "/static/js/2.a580c281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71ce73f3189640ef8809",
+    "url": "/static/js/main.d14d507d.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "ff1bc2486f0f5fde5a2d2174544ded03",
+    "url": "/static/media/Calculator.ff1bc248.png"
+  },
+  {
+    "revision": "05c93edd8a14af031d8c3c3d87e33b1b",
+    "url": "/static/media/Github.05c93edd.png"
+  },
+  {
+    "revision": "992cd7b910d3eeb6004e19aed09b1242",
+    "url": "/static/media/Headshot.992cd7b9.png"
+  },
+  {
+    "revision": "1614219cd2685cead2bcfcb4107e5f53",
+    "url": "/static/media/MapleBay.1614219c.png"
+  },
+  {
+    "revision": "5ec6ac90bea42345aeb16589b0b55323",
+    "url": "/static/media/PiKapp.5ec6ac90.png"
+  },
+  {
+    "revision": "4f2285d789b530ebf39a58bc6abdf0a7",
+    "url": "/static/media/responsiveGrid.4f2285d7.png"
+  }
+]);
